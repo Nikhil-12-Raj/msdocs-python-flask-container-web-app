254bdef1da33cb90836a32e58a078ba62ff2685a
Carl Rabeler <201999316+carlrabeler42@users.noreply.github.com>
2025-03-25 11:44:56 -0700

    Update production.py

--- azureproject/production.py
+++ azureproject/production.py
@@ -1,7 +1,8 @@
 import os
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'flask-insecure-7ppocbnx@w71dcuinn*t^_mzal(t@o01v3fee27g%rg18fc5d@'
+# SECRET_KEY = 'flask-insecure-7ppocbnx@w71dcuinn*t^_mzal(t@o01v3fee27g%rg18fc5d@'
+# SECRET_KEY not needed for this web app
 
 DEBUG = False
 ALLOWED_HOSTS = [os.environ['WEBSITE_HOSTNAME']] if 'WEBSITE_HOSTNAME' in os.environ else []
